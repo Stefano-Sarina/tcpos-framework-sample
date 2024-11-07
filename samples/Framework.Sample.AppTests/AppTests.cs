144efb41bf33d57ffa52bf7eb01bf808a49659bc
Stefano <stefano@sarina.it>
2024-11-07 22:01:00 +0100

    Add HttpClient and OData utilities, update batch handling
    Introduced new `HttpClientExtensions` and `ODataUtils` classes to
    handle HTTP operations and OData queries. Updated `AppTests` to
    use these new methods and added new test cases. Modified batch
    operation routes and refactored related classes to streamline
    batch handling. Removed redundant code and consolidated methods
    into the `Framework.Sample.AppTests.Helpers` namespace.

--- samples/Framework.Sample.AppTests/AppTests.cs
+++ samples/Framework.Sample.AppTests/AppTests.cs
@@ -1,9 +1,14 @@
-﻿using Framework.Sample.App;
+﻿using FluentAssertions;
+using FluentAssertions.Equivalency;
+using Framework.Sample.App;
 using Framework.Sample.App.Payloads;
+using Framework.Sample.AppTests.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Hosting;
+using TCPOS.AspNetCore.DataBind.Batch.Enums;
+using TCPOS.AspNetCore.DataBind.Payloads;
 using Xunit;
 
 namespace Framework.Sample.AppTests;
@@ -15,7 +20,7 @@ public class AppTests : IDisposable, IAsyncDisposable
 
     public AppTests()
     {
-        _webApplication = WebApplicationFactory.Create(new string[0], new App.WebApplicationFactoryOptions(true)).Result;
+        _webApplication = WebApplicationFactory.Create(Array.Empty<string>(), new App.WebApplicationFactoryOptions(true)).Result;
         _webApplication.Start();
         _httpClient = _webApplication.GetTestClient();
     }
@@ -32,16 +37,83 @@ public class AppTests : IDisposable, IAsyncDisposable
         _httpClient.Dispose();
     }
 
+
     [Fact]
     public async Task BatchCreateShouldWork()
     {
-        var batchId= await _httpClient.HttpPostAsync<object,string>("/api/1.0/Batch/1/200000", new object(),System.Net.HttpStatusCode.Created);
-        await _httpClient.HttpPostAsync<CustomerIn, string>($"/api/1.0/Batch/{batchId}/1/Customer/Insert", new CustomerIn()
+        var customers = await _httpClient.ODataHttpGetAsync<CustomerOut>($"/api/1.0/Customer",  System.Net.HttpStatusCode.OK);
+        foreach (var customer in customers)
+        {
+            await _httpClient.HttpDeleteAsync($"/api/1.0/Customer/{customer.Id}", System.Net.HttpStatusCode.OK);
+        }
+
+        var customerIn = new CustomerIn()
         {
             FirstName = "Dario",
             LastName = "Rossi"
-        }, System.Net.HttpStatusCode.Created);
-        await _httpClient.HttpPutAsync<object, string>($"/api/1.0/Batch/{batchId}", new object(), System.Net.HttpStatusCode.OK);
-        var arr=await _httpClient.HttpGetAsync<CustomerOut[]>($"/api/1.0/Customer", new QueryString(),System.Net.HttpStatusCode.OK);
+        };
+        await _httpClient. RunBatch(1, 20000, async batchId =>
+        {
+            await _httpClient.HttpPostAsync<CustomerIn, string>($"/api/1.0/Batch/{batchId}/1/Customer/{Operations.Insert}", customerIn, System.Net.HttpStatusCode.Created);
+        });
+
+        customers = await _httpClient.ODataHttpGetAsync<CustomerOut>($"/api/1.0/Customer", v => v.FirstName == customerIn.FirstName && v.LastName == customerIn.LastName, System.Net.HttpStatusCode.OK);
+        customers.Should().HaveCount(1);
+
+        customerIn = new CustomerIn()
+        {
+            FirstName = "Mario",
+            LastName = "Bianchi"
+        };
+        await _httpClient.RunBatch(1, 20000, async batchId =>
+        {
+            await _httpClient.HttpPostAsync<CustomerIn, string>($"/api/1.0/Batch/{batchId}/1/Customer/{customers.First().Id}/{Operations.Replace}", customerIn, System.Net.HttpStatusCode.Created);
+        });
+
+        customers = await _httpClient.ODataHttpGetAsync<CustomerOut>($"/api/1.0/Customer", v => v.FirstName == customerIn.FirstName && v.LastName == customerIn.LastName, System.Net.HttpStatusCode.OK);
+        customers.Should().HaveCount(1);
+
+        await _httpClient.RunBatch(1, 20000, async batchId =>
+        {
+            await _httpClient.HttpPostAsync<CustomerIn, string>($"/api/1.0/Batch/{batchId}/1/Customer/{customers.First().Id}/{Operations.Remove}", customerIn, System.Net.HttpStatusCode.Created);
+        });
+
+        customers = await _httpClient.ODataHttpGetAsync<CustomerOut>($"/api/1.0/Customer",  System.Net.HttpStatusCode.OK);
+        customers.Should().HaveCount(0);
     }
+
+
+    [Fact]
+    public async Task BatchCreateShouldWork1()
+    {
+        var customers = await _httpClient.ODataHttpGetAsync<CustomerOut>($"/api/1.0/Customer", System.Net.HttpStatusCode.OK);
+        foreach (var customer in customers)
+        {
+            await _httpClient.HttpDeleteAsync($"/api/1.0/Customer/{customer.Id}", System.Net.HttpStatusCode.OK);
+        }
+        var customerIn = new CustomerIn()
+        {
+            FirstName = "Dario",
+            LastName = "Rossi"
+        };
+        await _httpClient.RunBatch(1, 20000, async batchId =>
+        {
+            await _httpClient.HttpPostAsync<CustomerIn, string>($"/api/1.0/Batch/{batchId}/1/Customer/{Operations.Insert}", customerIn, System.Net.HttpStatusCode.Created);
+        });
+
+        customers = await _httpClient.ODataHttpGetAsync<CustomerOut>($"/api/1.0/Customer", v => v.FirstName == customerIn.FirstName && v.LastName == customerIn.LastName, System.Net.HttpStatusCode.OK);
+        customers.Should().HaveCount(1);
+
+        var orderIn = new OrderIn<ValueReference>()
+        {
+            CustomerId = ValueReference.CreateValue(customers.First().Id),
+            Notes = "Test",
+            OrderDate = DateOnly.FromDateTime( DateTime.Now)
+        };
+        await _httpClient.RunBatch(1, 20000, async batchId =>
+        {
+            await _httpClient.HttpPostAsync<OrderIn<ValueReference>, string>($"/api/1.0/Batch/{batchId}/1/Order/{Operations.Insert}", orderIn, System.Net.HttpStatusCode.Created);
+        });
+    }
+
 }
