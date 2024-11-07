02b36ec358a7e9a31a4578893ab91f3865ebb730
Stefano Sarina <stefano.sarina@zucchetti.com>
2024-11-07 17:41:51 +0100

    Add new test project and refactor for testing
    Added `Framework.Sample.AppTests` project to the solution for testing purposes. Updated solution configuration to include build configurations for the new test project. Created `AppTests` class in `AppTests.cs` using `Xunit` with a sample test method `TestMethod1`. Added `Framework.Sample.AppTests.csproj` with dependencies like `FluentAssertions`, `Microsoft.NET.Test.Sdk`, and `xunit`. Added `launchSettings.json` for the test project.
    
    Created `WebApplicationFactory` class in `WebApplicationFactory.cs` to set up and configure the web application for testing, including authentication, Swagger, and database setup. Moved application setup and configuration logic from `Program.cs` to `WebApplicationFactory.cs`.
    
    Updated `Framework.Sample.App.csproj` to reference `Microsoft.AspNetCore.TestHost`. Removed setup and configuration logic from `Program.cs`, replacing it with a call to `WebApplicationFactory.Create`.
    
    Refactored `RouteConfigurationDataByRouteValues` to remove unnecessary line breaks. Updated `ConfigurationExtensions` to make `action` parameter nullable and removed null checks, using the null-conditional operator instead. Modified `UseDataBind` to remove null checks for `batchRouteMapperConfigurationAction` and `bataPullOutRouteMapperConfigurationAction`.

---
+++ samples/Framework.Sample.App/WebApplicationFactory.cs
@@ -0,0 +1,158 @@
+﻿using System.Security.Claims;
+using AutoMapper;
+using Framework.Sample.App.Configuration;
+using Framework.Sample.App.DataBind;
+using Framework.Sample.App.DB;
+using Framework.Sample.App.DB.Entities;
+using Framework.Sample.App.Payloads;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using TCPOS.AspNetCore.DataBind.Configuration;
+using TCPOS.AspNetCore.DataBind.Implementations.Batches;
+using TCPOS.AspNetCore.DataBind.Implementations.OData.DataPullOut;
+using TCPOS.AspNetCore.DataBind.Implementations.OData.Interfaces;
+using TCPOS.AspNetCore.DataBind.Implementations.Routes;
+using TCPOS.AspNetCore.DataBind.Payloads;
+
+namespace Framework.Sample.App;
+
+public record WebApplicationOptions(bool UseTestServer);
+
+public static class WebApplicationFactory
+{
+    private const string Admin = "Admin";
+    private const string User = "User";
+
+    public static async Task<Microsoft.AspNetCore.Builder.WebApplication> Create(string[] args, WebApplicationOptions webApplicationOptions=null)
+    {
+        var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddJsonFile("appsettings.json", false, true);
+
+        if (webApplicationOptions?.UseTestServer??false)
+        {
+            builder.WebHost.UseTestServer();
+        }
+
+        ConfigureServices(builder.Services);
+
+        var application = builder.Build();
+
+        ConfigureApplication(application);
+
+        var cfg = application.Configuration.Get<Configuration.Configuration>();
+
+        if (cfg?.Debug?.CreateDatabase ?? false)
+        {
+            using var scope = application.Services.CreateScope();
+            await scope.ServiceProvider.GetRequiredService<SampleDbContext>().Database.EnsureDeletedAsync();
+            await scope.ServiceProvider.GetRequiredService<SampleDbContext>().Database.EnsureCreatedAsync();
+        }
+
+        if (cfg?.Debug?.FillDemoData ?? false)
+        {
+            using var scope = application.Services.CreateScope();
+            var ctx = scope.ServiceProvider.GetRequiredService<SampleDbContext>();
+            //ctx.Products.InsertOrUpdate(i=>i.Name=="");
+        }
+
+        return application;
+    }
+
+    private static void ConfigureApplication(Microsoft.AspNetCore.Builder.WebApplication webApplication)
+    {
+        webApplication.UseAuthentication();
+
+        webApplication.UseSwagger();
+        webApplication.UseSwaggerUI();
+        webApplication.UseDataBind();
+        webApplication.MapPost("/api/login", async (HttpContext httpContext, [FromQuery] bool isAdmin) =>
+        {
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Name, isAdmin ? Admin : User),
+                new(ClaimTypes.Role, isAdmin ? Admin : User)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties();
+            await httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+            return Results.Created();
+        });
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+
+        typeof(Program).Assembly.GetTypes().Where(x => x is { IsClass: true, IsAbstract: false } && x.IsSubclassOf(typeof(Profile)))
+                       .ToList()
+                       .ForEach(x => services.AddAutoMapper(x));
+
+        services.AddSingleton<IEdmModelBuilder, DataEntityEdmModelBuilder>();
+
+        services.AddDbContext<SampleDbContext>((s, o) =>
+        {
+            var cfg = s.GetRequiredService<IConfiguration>().Get<Configuration.Configuration>();
+
+            switch (cfg?.DatabaseConnection?.DatabaseType)
+            {
+                case DatabaseTypes.SqlServer:
+                    o.UseSqlServer(cfg?.DatabaseConnection.ConnectionString);
+                    break;
+                case DatabaseTypes.Sqlite:
+                    o.UseSqlite(cfg?.DatabaseConnection.ConnectionString);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddDataBind(c =>
+        {
+            c.AddStorageProvider<StorageProvider>();
+            c.AddRouteConfigurationData<RouteConfigurationDataByRouteValues>();
+
+            c.AddDataPullOuts()
+              //Customer
+             .AddDataPullOutItem<DbContextDataPullOutItem<Customer, CustomerOut>>()
+              //Order
+             .AddDataPullOutItem<DbContextDataPullOutItem<Order, OrderOut<int>>>()
+              //OrderDetail
+             .AddDataPullOutItem<DbContextDataPullOutItem<OrderDetail, OrderDetailOut<int>>>()
+              //Product
+             .AddDataPullOutItem<DbContextDataPullOutItem<Product, ProductOut>>()
+                ;
+
+            c.AddBatches<InMemoryBatchStorage>()
+              //Customer
+             .AddBatchItem<DbContextTypedPostBatchCommand<Customer, CustomerIn, CustomerIn>>()
+             .AddBatchItem<DbContextTypedPutBatchCommand<Customer, CustomerIn, CustomerIn>>()
+             .AddBatchItem<DbContextTypedPatchBatchCommand<Customer, CustomerIn, CustomerIn>>()
+             .AddBatchItem<DbContextTypedDeleteBatchCommand<Customer>>()
+              //Order
+             .AddBatchItem<DbContextTypedPostBatchCommand<Order, OrderIn<int>, OrderIn<ValueReference>>>()
+             .AddBatchItem<DbContextTypedPutBatchCommand<Order, OrderIn<int>, OrderIn<ValueReference>>>()
+             .AddBatchItem<DbContextTypedPatchBatchCommand<Order, OrderIn<int>, OrderIn<ValueReference>>>()
+             .AddBatchItem<DbContextTypedDeleteBatchCommand<Order>>()
+              //OrderDetail
+             .AddBatchItem<DbContextTypedPostBatchCommand<OrderDetail, OrderDetailIn<int>, OrderDetailIn<ValueReference>>>()
+             .AddBatchItem<DbContextTypedPutBatchCommand<OrderDetail, OrderDetailIn<int>, OrderDetailIn<ValueReference>>>()
+             .AddBatchItem<DbContextTypedPatchBatchCommand<OrderDetail, OrderDetailIn<int>, OrderDetailIn<ValueReference>>>()
+             .AddBatchItem<DbContextTypedDeleteBatchCommand<OrderDetail>>()
+              //Product
+             .AddBatchItem<DbContextTypedPostBatchCommand<Product, ProductIn, ProductIn>>()
+             .AddBatchItem<DbContextTypedPutBatchCommand<Product, ProductIn, ProductIn>>()
+             .AddBatchItem<DbContextTypedPatchBatchCommand<Product, ProductIn, ProductIn>>()
+             .AddBatchItem<DbContextTypedDeleteBatchCommand<Product>>()
+                ;
+        });
+    }
+}
---
+++ samples/Framework.Sample.AppTests/AppTests.cs
@@ -0,0 +1,39 @@
+﻿using Framework.Sample.App;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Framework.Sample.AppTests;
+
+public class AppTests : IDisposable, IAsyncDisposable
+{
+    private readonly HttpClient _httpClient;
+    private readonly Microsoft.AspNetCore.Builder.WebApplication _webApplication;
+
+    public AppTests()
+    {
+        _webApplication = App.WebApplicationFactory.Create(new string[0], new App.WebApplicationOptions(true)).Result;
+        _webApplication.Start();
+        _httpClient = _webApplication.GetTestClient();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _webApplication.DisposeAsync();
+        _httpClient.Dispose();
+    }
+
+    public void Dispose()
+    {
+        ((IDisposable)_webApplication).Dispose();
+        _httpClient.Dispose();
+    }
+
+    [Fact]
+    public async Task TestMethod1()
+    {
+        var aaaa=await _httpClient.PostAsync("/api/1.0/Batch/1/20000", null);
+        aaaa.EnsureSuccessStatusCode();
+    }
+}
