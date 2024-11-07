14fcf18f50a183b8741629ae6467acfda893243b
Stefano Sarina <stefano.sarina@zucchetti.com>
2024-11-07 13:53:52 +0100

    Update .gitignore, Publish.ps1, and add new classes
    Updated .gitignore to include patterns for Sample.App.zip and .config files.
    Modified Publish.ps1 to clean, publish, and compress the project.
    Added TCPOS.AspNetCore.DataBind.Implementations.Interfaces namespace to several files.
    Changed namespace of IDEntity.cs.
    Added new classes RouteConfigurationDataByRouteValues and RouteData<T>.
    Added new methods to HttpExtensions.cs for route value handling.
    Removed redundant using statement from HttpExtensions.cs.

---
+++ samples/Framework.Sample.App/Publish.ps1
@@ -0,0 +1,15 @@
+﻿if(Test-Path $PSScriptRoot/Publish)
+{
+    Remove-Item $PSScriptRoot/Publish -Force -Recurse
+}
+if(Test-Path $PSScriptRoot/Sample.App.zip)
+{
+    Remove-Item $PSScriptRoot/Sample.App.zip -Force -Recurse
+}
+
+& dotnet publish $PSScriptRoot/Framework.Sample.App.csproj -o $PSScriptRoot/Publish --sc -r win-x64
+
+Remove-Item $PSScriptRoot/Publish/*.pdb -Force -Recurse
+Remove-Item $PSScriptRoot/Publish/appsettings.json -Force -Recurse
+
+Compress-Archive $PSScriptRoot/Publish/*.* $PSScriptRoot/Sample.App.zip
