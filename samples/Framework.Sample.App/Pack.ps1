8e414748210e5db076bf195e2d4f8d0ade5add11
Stefano <stefano@sarina.it>
2024-11-08 00:37:00 +0100

    Fix paths in script and update dotnet pack commands
    Corrected the path in the `Remove-Item` command from `$PSScriptRoot/Package` to `$PSScriptRoot/Packages` to ensure the correct directory is removed. Updated `dotnet pack` commands to use relative paths (`..\..`) to target the correct project files.

--- samples/Framework.Sample.App/Pack.ps1
+++ samples/Framework.Sample.App/Pack.ps1
@@ -1,15 +1,14 @@
 ﻿if(Test-Path $PSScriptRoot/Packages)
 {
-    Remove-Item $PSScriptRoot/Package -Force -Recurse
+    Remove-Item $PSScriptRoot/Packages -Force -Recurse
 }
 # Remove-Item $PSScriptRoot/Sample.App.*.zip -Force -Recurse
 
 & dotnet pack $PSScriptRoot\..\..\src\TCPOS.AspNetCore.DataBind\TCPOS.AspNetCore.DataBind.csproj -o $PSScriptRoot/Packages --include-symbols
-# & dotnet pack $PSScriptRoot\src\TCPOS.AspNetCore.DataBind.Implementations\TCPOS.AspNetCore.DataBind.Implementations.csproj -o $PSScriptRoot/Packages --include-symbols
-# & dotnet pack $PSScriptRoot\src\TCPOS.EntityFramework\TCPOS.EntityFramework.csproj -o $PSScriptRoot/Packages --include-symbols
-# & dotnet pack $PSScriptRoot\src\TCPOS.Common.CommandLine\TCPOS.Common.CommandLine.csproj -o $PSScriptRoot/Packages --include-symbols
-# & dotnet pack $PSScriptRoot\src\TCPOS.Common\TCPOS.Common.csproj -o $PSScriptRoot/Packages --include-symbols
-# & dotnet pack $PSScriptRoot\src\TCPOS.Common\TCPOS.Common.csproj -o $PSScriptRoot/Packages --include-symbols
+& dotnet pack $PSScriptRoot\..\..\src\TCPOS.AspNetCore.DataBind.Implementations\TCPOS.AspNetCore.DataBind.Implementations.csproj -o $PSScriptRoot/Packages --include-symbols
+& dotnet pack $PSScriptRoot\..\..\src\TCPOS.EntityFramework\TCPOS.EntityFramework.csproj -o $PSScriptRoot/Packages --include-symbols
+& dotnet pack $PSScriptRoot\..\..\src\TCPOS.Common\TCPOS.Common.csproj -o $PSScriptRoot/Packages --include-symbols
+& dotnet pack $PSScriptRoot\..\..\src\TCPOS.Common.CommandLine\TCPOS.Common.CommandLine.csproj -o $PSScriptRoot/Packages --include-symbols
 
 # Remove-Item $PSScriptRoot/Publish/*.pdb -Force -Recurse
 # Remove-Item $PSScriptRoot/Publish/appsettings.json -Force -Recurse
