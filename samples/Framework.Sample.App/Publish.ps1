15451d9d086016e1e4ee82a9fc04a18f3dfc4323
Stefano Sarina <stefano.sarina@zucchetti.com>
2024-11-07 14:02:24 +0100

    Enhance zip file handling and naming
    The script now removes any zip files matching the pattern `Sample.App.*.zip` instead of just `Sample.App.zip`. Additionally, the output zip file name now includes a timestamp in the format `yyyyMMddHHmm` to ensure uniqueness.

--- samples/Framework.Sample.App/Publish.ps1
+++ samples/Framework.Sample.App/Publish.ps1
@@ -2,14 +2,11 @@
 {
     Remove-Item $PSScriptRoot/Publish -Force -Recurse
 }
-if(Test-Path $PSScriptRoot/Sample.App.zip)
-{
-    Remove-Item $PSScriptRoot/Sample.App.zip -Force -Recurse
-}
+Remove-Item $PSScriptRoot/Sample.App.*.zip -Force -Recurse
 
 & dotnet publish $PSScriptRoot/Framework.Sample.App.csproj -o $PSScriptRoot/Publish --sc -r win-x64
 
 Remove-Item $PSScriptRoot/Publish/*.pdb -Force -Recurse
 Remove-Item $PSScriptRoot/Publish/appsettings.json -Force -Recurse
 
-Compress-Archive $PSScriptRoot/Publish/*.* $PSScriptRoot/Sample.App.zip
+Compress-Archive $PSScriptRoot/Publish/*.* "$PSScriptRoot/Sample.App.$(Get-Date -format "yyyyMMddHHmm").zip"
