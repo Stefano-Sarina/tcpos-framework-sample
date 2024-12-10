5f330875944662cdfd23f120d6f240ffbe5be44e
Stefano Sarina <stefano.sarina@zucchetti.com>
2024-12-10 13:11:55 +0100

    Update project files with versioning and company details
    Updated various .csproj files to include new properties for
    AssemblyVersion, FileVersion, Version, IncludeSourceRevisionInInformationalVersion,
    Company, and Copyright. These changes set the versioning
    information and company details for the assemblies.
    
    Modified Pack.ps1 script to include a new dotnet pack command
    for the TCPOS.Authorization project, ensuring it is packed
    along with other projects.

--- samples/Framework.Sample.App/Pack.ps1
+++ samples/Framework.Sample.App/Pack.ps1
@@ -9,6 +9,7 @@
 & dotnet pack $PSScriptRoot\..\..\src\TCPOS.EntityFramework\TCPOS.EntityFramework.csproj -o $PSScriptRoot/Packages --include-symbols
 & dotnet pack $PSScriptRoot\..\..\src\TCPOS.Common\TCPOS.Common.csproj -o $PSScriptRoot/Packages --include-symbols
 & dotnet pack $PSScriptRoot\..\..\src\TCPOS.Common.CommandLine\TCPOS.Common.CommandLine.csproj -o $PSScriptRoot/Packages --include-symbols
+& dotnet pack $PSScriptRoot\..\..\..\tcpos-authorization\src\TCPOS.Authorization\TCPOS.Authorization.csproj -o $PSScriptRoot/Packages --include-symbols
 
 # Remove-Item $PSScriptRoot/Publish/*.pdb -Force -Recurse
 # Remove-Item $PSScriptRoot/Publish/appsettings.json -Force -Recurse
